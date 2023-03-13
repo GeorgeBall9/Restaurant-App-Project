bd9209e3d94b486b9ec815233108e49b38a25273
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-03-13 20:43:06 +0000

    // removing comment from commit test

--- src/common/components/RestaurantCard/RestaurantCard.jsx
+++ src/common/components/RestaurantCard/RestaurantCard.jsx
@@ -1,7 +1,5 @@
 import "./RestaurantCard.css";
 
-// git commit TEST --George
-
 // do not display id in the dom - it is just there in case we want to add a click function
 const RestaurantCard = ({id, name, rating, distance, price, primaryCuisine, photoUrl}) => {
     return (

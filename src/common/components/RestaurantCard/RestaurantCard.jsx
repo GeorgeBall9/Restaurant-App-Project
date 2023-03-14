f15d0cceebd7c5a5fbbc4f55b1008410d2fd191f
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-03-14 16:36:25 +0000

    Restaurant Card styling

--- src/common/components/RestaurantCard/RestaurantCard.jsx
+++ src/common/components/RestaurantCard/RestaurantCard.jsx
@@ -1,63 +1,71 @@
 import "./RestaurantCard.css";
-import {faCircle, faLocationArrow, faStar, faSterlingSign, faUtensils} from "@fortawesome/free-solid-svg-icons";
+import {faLocationArrow, faStar, faStarHalfStroke, faSterlingSign, faUtensils} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
+
+// do not display id in the dom - it is just there in case we want to add a click function
+
+// A card component for displaying restaurant information
 const RestaurantCard = ({id, name, rating, distance, price, primaryCuisine, photoUrl}) => {
 
+    // Convert number rating into star representaion on the restaurant card
+    const starRating = Math.round(rating * 2) / 2; // round to nearest half
+    const fullStars = Math.floor(starRating);
+    const halfStar = starRating - fullStars > 0;
+    const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
+    const starIcons = [];
+    
+    // Create an arry of FontAwesomeIcons to display the star rating
+    for (let i = 0; i < fullStars; i++) {
+        starIcons.push(<FontAwesomeIcon key={i} icon={faStar} />);
+    }
+
+    if (halfStar) {
+        starIcons.push(<FontAwesomeIcon key="half" icon={faStarHalfStroke} half="true" />);
+    }
+
+    for (let i = 0; i < emptyStars; i++) {
+        starIcons.push(<FontAwesomeIcon key={i + fullStars + (halfStar ? 1 : 0)} icon={faStar} regular="true" />);
+    }
+
+    // Create the price display if the price is unknown
+    const priceDisplay = price !== "Unknown" ? (
+        <div className="restaurant-card-price">
+          <FontAwesomeIcon icon={faSterlingSign}></FontAwesomeIcon>
+          <span>{price}</span>
+          <span className="restaurant-card-cuisine">{primaryCuisine}</span>
+        </div>
+      ) : null;
+    
+    // Render the component
     return (
       <div className="restaurant-card-container">
-        <div className="restaurant-card-image-container">
-          <img src={ photoUrl } alt={ name } className="restaurant-card-image" />
-        </div>
+        
         <div className="restaurant-card-details">
-          <h3 className="restaurant-card-name">{ name }</h3>
-          <div className="restaurant-card-rating">
-            <FontAwesomeIcon icon={ faStar }></FontAwesomeIcon>
-            <span>{ rating }</span>
-            <span className="restaurant-card-distance">{ distance } mi</span>
+          <h3 className="restaurant-card-name">{name}</h3>
+            <div className="restaurant-card-rating">
+            {starIcons}
+            <span>{rating}</span>
           </div>
+          <div className="restaurant-card-distance">
+            <FontAwesomeIcon icon={faLocationArrow} />{distance} km
+            </div>
           <div className="restaurant-card-price">
-            <FontAwesomeIcon icon={ faSterlingSign }></FontAwesomeIcon>
-            <span>{ price }</span>
-            <span className="restaurant-card-cuisine">{ primaryCuisine }</span>
+            {priceDisplay}
+            <span className="restaurant-card-cuisine">{primaryCuisine}</span>
           </div>
         </div>
         <div className="restaurant-card-location">
-          <FontAwesomeIcon icon={ faLocationArrow } />
+          <FontAwesomeIcon icon={faLocationArrow} />
         </div>
-      </div>
-    );
-  };
-
-  // n amount of restaurant cards
-
-  const RestaurantCardList = () => {
-    return (
-      <div>
-        {data.map((restaurant) => (
-          <RestaurantCard
-            key={restaurant.id}
-            //id={restaurant.id}
-            name={restaurant.name}
-            rating={restaurant.rating}
-            distance={restaurant.distance}
-            price={restaurant.price}
-            primaryCuisine={restaurant.primaryCuisine}
-            photoUrl={restaurant.photoUrl}
-          />
-        ))}
+        <div className="restaurant-card-image-container">
+          <img src={photoUrl} alt={name} className="restaurant-card-image" />
+        </div>
       </div>
     );
   };
 
 
 export default RestaurantCard;
-// do not display id in the dom - it is just there in case we want to add a click function
-// const RestaurantCard = ({id, name, rating, distance, price, primaryCuisine, photoUrl}) => {
-//     return (
-//         <div className="restaurant-card-container">
-//             <FontAwesomeIcon icon={faLocationArrow} />
-//             <h2>{ name } </h2>
-//         </div>
-//     );
-// };
+
+

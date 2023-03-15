4c618edcfb5d5174b1aab79d319f0ef40c356c41
ryhhill1998 <ryhhill1998@outlook.com>
2023-03-15 09:55:02 +0000

    Changed css units from px to em and rem for better responsiveness. Changed css selector units and method of selection in stylesheet to make change to sass easier later in project

--- src/common/components/RestaurantCard/RestaurantCard.jsx
+++ src/common/components/RestaurantCard/RestaurantCard.jsx
@@ -9,7 +9,13 @@ import "./RestaurantCard.css";
 
 // FontAwesome icons
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faLocationArrow, faMapLocationDot, faStar, faStarHalfStroke, faSterlingSign} from "@fortawesome/free-solid-svg-icons";
+import {
+    faLocationArrow,
+    faMapLocationDot,
+    faStar,
+    faStarHalfStroke,
+    faSterlingSign
+} from "@fortawesome/free-solid-svg-icons";
 import {faStar as faEmptyStar} from "@fortawesome/free-regular-svg-icons";
 
 // do not display id in the dom - it is just there in case we want to add a click function
@@ -22,52 +28,52 @@ const RestaurantCard = ({id, name, rating, distance, price, primaryCuisine, phot
     const fullStars = Math.floor(starRating);
     const halfStar = starRating - fullStars > 0;
     const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
-    
-    
+
+
     // Render the component
     return (
         <div className="restaurant-card-container">
-            <div className="restaurant-card-details">
-                <h3 className="restaurant-card-name">{name}</h3>
+            <div className="details-container">
+                <h3>{name}</h3>
 
-                <div className="restaurant-card-rating">
+                <div className="rating-container">
                     {[...Array(fullStars)].map((star, i) => (
-                        <FontAwesomeIcon key={i} icon={faStar}/>
+                        <FontAwesomeIcon key={i} icon={faStar} className="icon"/>
                     ))}
 
-                    {halfStar && <FontAwesomeIcon icon={faStarHalfStroke}/>}
+                    {halfStar && <FontAwesomeIcon icon={faStarHalfStroke} className="icon"/>}
 
                     {emptyStars > 0 && [...Array(emptyStars)].map((star, i) => (
-                        <FontAwesomeIcon key={i} icon={faEmptyStar}/>
+                        <FontAwesomeIcon key={i} icon={faEmptyStar} className="icon"/>
                     ))}
 
                     <span>{starRating}</span>
                 </div>
 
-                <div className="restaurant-card-distance">
-                    <FontAwesomeIcon icon={faLocationArrow}/>{distance} km
+                <div className="distance-container">
+                    <FontAwesomeIcon icon={faLocationArrow} className="icon"/>
+                    {distance} km
                 </div>
 
-                <div className="restaurant-card-price">
+                <div className="price-cuisine-container">
 
                     {price !== "Unknown" && (
-                        <div className="restaurant-card-price">
-                            <FontAwesomeIcon icon={faSterlingSign}/>
-                            <span>{price}</span>
-                            <span className="restaurant-card-cuisine">{primaryCuisine}</span>
+                        <div className="price-container">
+                            <FontAwesomeIcon icon={faSterlingSign} className="icon"/>
+                            {price}
                         </div>
                     )}
 
-                    <span className="restaurant-card-cuisine">{primaryCuisine}</span>
+                    <span className="cuisine">{primaryCuisine}</span>
                 </div>
             </div>
 
-            <div className="restaurant-card-location">
-                <FontAwesomeIcon icon={faLocationArrow}/>
-            </div>
+            <div className="container-rhs">
+                <FontAwesomeIcon icon={faMapLocationDot} className="icon"/>
 
-            <div className="restaurant-card-image-container">
-                <img src={photoUrl} alt={name} className="restaurant-card-image"/>
+                <div className="image-container">
+                    <img src={photoUrl} alt={name}/>
+                </div>
             </div>
         </div>
     );

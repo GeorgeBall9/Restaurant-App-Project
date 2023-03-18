87d20c683dbdc2ee9ea3bf9b8326ee3f54590ae7
ryhhill1998 <ryhhill1998@outlook.com>
2023-03-18 16:39:20 +0000

    Changed css to prevent restaurant name taking up more than two lines on card preview

--- src/common/components/RestaurantCard/RestaurantCard.jsx
+++ src/common/components/RestaurantCard/RestaurantCard.jsx
@@ -21,15 +21,11 @@ const RestaurantCard = ({restaurant, openingHours, view, style, handleClick}) =>
     // Convert number rating into star representation on the restaurant card
     const starRating = Math.round(rating * 2) / 2; // round to nearest half
 
-    const icon = view === "map" ? faRoute : faBookmark;
-
-    const displayedName = name.substring(0, 30).trim() + (name.length > 30 ? "..." : "");
-
     // Render the component
     return (
         <div className="restaurant-card" style={style}>
             <div className="details-container">
-                <h3>{displayedName}</h3>
+                <h3>{name}</h3>
 
                 <StarRating rating={starRating}/>
 

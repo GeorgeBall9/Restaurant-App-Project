eee8bd09431dba0f5388f198cc3cb4a4746c7eac
ryhhill1998 <ryhhill1998@outlook.com>
2023-03-25 23:20:31 +0000

    Added smoother functionality to slider

--- src/features/slider/Slider/Slider.jsx
+++ src/features/slider/Slider/Slider.jsx
@@ -3,7 +3,7 @@ import RestaurantsList from "../../restaurants/RestaurantsList/RestaurantsList";
 import {useSwipeable} from "react-swipeable";
 import {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {changeSlide, selectActiveSlide} from "../sliderSlice";
+import {changeSlide, selectActiveSlide, setActiveSlide} from "../sliderSlice";
 import {displayRestaurant} from "../../map/mapSlice";
 import {selectRestaurants} from "../../restaurants/restaurantsSlice";
 
@@ -17,17 +17,44 @@ const Slider = () => {
     const offset = (window.innerWidth - position) / 2;
 
     const [xPosition, setXPosition] = useState(offset);
+    const [offsetX, setOffsetX] = useState(0);
 
     const handlers = useSwipeable({
-        onSwipedLeft: () => dispatch(changeSlide("forward")),
-        onSwipedRight: () => dispatch(changeSlide("backward")),
+        // onSwipedLeft: () => dispatch(changeSlide("forward")),
+        // onSwipedRight: () => dispatch(changeSlide("backward")),
         onSwiping: ({deltaX}) => {
             setStyle(style => {
                 const updatedStyle = {...style};
                 const translateX = xPosition + deltaX;
+                setOffsetX(deltaX);
                 updatedStyle.transform = `translateX(${translateX}px)`;
                 return updatedStyle;
             });
+        },
+        onTouchEndOrOnMouseUp: () => {
+            const magnitude = Math.abs(offsetX);
+
+            if (xPosition === 0 && offsetX > 0) {
+                setStyle(style => {
+                    const updatedStyle = {...style};
+                    const translateX = offset - (activeSlide * position);
+                    updatedStyle.transform = `translateX(${translateX}px)`;
+                    setXPosition(translateX);
+                    return updatedStyle;
+                });
+            } else if (offsetX < 0 && magnitude > 0.25 * position) {
+                dispatch(changeSlide("forward"));
+            } else if (magnitude > 0.25 * position) {
+                dispatch(changeSlide("backward"));
+            } else {
+                setStyle(style => {
+                    const updatedStyle = {...style};
+                    const translateX = offset - (activeSlide * position);
+                    updatedStyle.transform = `translateX(${translateX}px)`;
+                    setXPosition(translateX);
+                    return updatedStyle;
+                });
+            }
         },
         preventScrollOnSwipe: true,
         trackMouse: true

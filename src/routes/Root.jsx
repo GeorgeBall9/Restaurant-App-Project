1a80b8996766e6755fd19984c94c3e2ec1065dc3
ryhhill1998 <ryhhill1998@outlook.com>
2023-03-26 11:57:58 +0100

    will need next js to circumvent cors policy blocker

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -7,8 +7,6 @@ import useFilterRestaurants from "../common/hooks/useFilterRestaurants";
 import FiltersDropdown from "../features/filters/FiltersDropdown/FiltersDropdown";
 import {selectFiltersAreVisible} from "../features/filters/filtersSlice";
 import useInitialiseSlider from "../common/hooks/useInitialiseSlider";
-import {useEffect} from "react";
-import {selectRestaurants} from "../features/restaurants/restaurantsSlice";
 
 const Root = () => {
 
@@ -18,35 +16,6 @@ const Root = () => {
 
     const spinnerIsVisible = useSelector(selectSpinnerIsVisible);
     const filtersVisible = useSelector(selectFiltersAreVisible);
-
-    const restaurants = useSelector(selectRestaurants);
-
-    // const options = {method: 'GET', headers: {accept: 'application/json'}};
-
-    useEffect(() => {
-        fetch('https://api.content.tripadvisor.com/api/v1/location/19769400/photos?' +
-            'key=&language=en')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("The requested resource is not available.");
-                }
-
-                return response.json();
-            })
-            .then(data => {
-                const dataArray = data.data;
-                const formattedData = dataArray.reduce((object, entry) => {
-                    if (!object.id) {
-                        object.id = entry.id;
-                    }
-
-                    object.photoUrls.push(entry.images?.original?.url);
-                    return object;
-                }, {id: null, photoUrls: []});
-                console.log(formattedData)
-            })
-            .catch(err => console.error(err));
-    }, [restaurants]);
 
     return (
         <>

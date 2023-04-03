86c491f9033b5803537e80488cf8aac3a9d88e3e
ryhhill1998 <ryhhill1998@outlook.com>
2023-04-03 17:56:56 +0100

    Initialised firebase.js file in app

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+// imports
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// firebase config
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+// initialise app
+const app = initializeApp(firebaseConfig);
+
+// initialise firestore database
+const db = getFirestore(app);

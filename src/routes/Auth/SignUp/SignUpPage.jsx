8230677926d0f5e9dacd4da74cca8dff5f19fbc2
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-04-04 11:39:07 +0100

    typo

--- src/routes/Auth/SignUp/SignUpPage.jsx
+++ src/routes/Auth/SignUp/SignUpPage.jsx
@@ -65,7 +65,7 @@ function SignUpPage() {
 
             <div className="signup-page-signin">
                 <p>Already have an account?
-                    <Link to="./login" className="signin-link"> Sign in.</Link>
+                    <Link to="/login" className="signin-link"> Sign in.</Link>
                 </p>
             </div>
         </div>

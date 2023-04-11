35c25ac0220486ff38b7f8aa1bd90b2e41ee4958
ryhhill1998 <ryhhill1998@outlook.com>
2023-04-11 08:04:37 +0100

    Implemented basics of bookmarks page

--- src/routes/Bookmarks/Bookmarks.jsx
+++ src/routes/Bookmarks/Bookmarks.jsx
@@ -1,10 +1,12 @@
 import "./Bookmarks.css";
 import {useSelector} from "react-redux";
 import {selectBookmarks, selectUserId} from "../../features/user/userSlice";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import RestaurantCard from "../../common/components/RestaurantCard/RestaurantCard";
+import {selectAllRestaurants} from "../../features/restaurants/restaurantsSlice";
 
 const Bookmarks = () => {
 
@@ -12,6 +14,9 @@ const Bookmarks = () => {
 
     const userId = useSelector(selectUserId);
     const userBookmarks = useSelector(selectBookmarks);
+    const allRestaurants = useSelector(selectAllRestaurants);
+
+    const [bookmarkedRestaurants, setBookmarkedRestaurants] = useState([]);
 
     useEffect(() => {
         if (!userId) {
@@ -19,12 +24,29 @@ const Bookmarks = () => {
         }
     }, [userId]);
 
+    useEffect(() => {
+        if (!userBookmarks || !allRestaurants) return;
+
+        console.log(userBookmarks)
+
+        setBookmarkedRestaurants(userBookmarks
+            .map(restaurantId => allRestaurants
+                .find(restaurant => restaurant.id === restaurantId))
+            .filter(restaurant => restaurant));
+    }, [userBookmarks, allRestaurants]);
+
     const handleBackClick = () => {
         navigate("/profile");
     };
 
+    useEffect(() => {
+        if (!bookmarkedRestaurants) return;
+
+        console.log(bookmarkedRestaurants)
+    }, [bookmarkedRestaurants]);
+
     return (
-        <div className="bookmarks-container container">
+        <div className="bookmarks-page-container container">
             <header>
                 <button onClick={handleBackClick}>
                     <FontAwesomeIcon className="icon" icon={faArrowLeft}/>
@@ -33,11 +55,17 @@ const Bookmarks = () => {
 
                 <h1>Bookmarks</h1>
 
-                <button  style={{visibility: "hidden"}}>
+                <button style={{visibility: "hidden"}}>
                     <FontAwesomeIcon className="icon" icon={faArrowLeft}/>
                     Back
                 </button>
             </header>
+
+            <div className="bookmarks-container">
+                {bookmarkedRestaurants.length > 0 && bookmarkedRestaurants.map(restaurant => (
+                    <RestaurantCard restaurant={restaurant}/>
+                ))}
+            </div>
         </div>
     );
 };

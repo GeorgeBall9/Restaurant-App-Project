540f8c55fe5831c50f938f4ffab0f7d6289a4cf2
ryhhill1998 <ryhhill1998@outlook.com>
2023-04-16 15:14:37 +0100

    Started adding chart to reviews section

--- src/routes/DetailsPage/ReviewsSection/ReviewsSection.jsx
+++ src/routes/DetailsPage/ReviewsSection/ReviewsSection.jsx
@@ -11,6 +11,68 @@ import StarRating from "../../../common/components/StarRating/StarRating";
 
 import sortImageSrc from "../../../common/images/sort.png";
 
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export const options = {
+    indexAxis: 'y',
+    elements: {
+        bar: {
+            borderWidth: 2,
+        },
+    },
+    responsive: true,
+    plugins: {
+        legend: {
+            display: false
+        }
+    },
+    scales: {
+        x: {
+            display: false
+        },
+        y: {
+            grid: {
+                display: false
+            },
+            border: {
+                display: false
+            }
+        }
+    }
+};
+
+const labels = [5, 4, 3, 2, 1];
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            data: [5, 6, 19, 3, 2],
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        }
+    ],
+};
+
 const ReviewsSection = ({userId, restaurant}) => {
 
     const restaurantId = restaurant?.id;
@@ -50,11 +112,15 @@ const ReviewsSection = ({userId, restaurant}) => {
             <h2>Reviews</h2>
 
             <div className="review-stats">
-                <h3>Overall rating</h3>
+                <div className="rating-container">
+                    <h3>Overall rating</h3>
 
-                <p>{restaurant.rating}</p>
+                    <p>{restaurant.rating}</p>
 
-                <StarRating rating={restaurant.rating} hideNumber={true}/>
+                    <StarRating rating={restaurant.rating} hideNumber={true}/>
+                </div>
+
+                <Bar options={options} data={data} />
             </div>
 
             <div className="search-container">

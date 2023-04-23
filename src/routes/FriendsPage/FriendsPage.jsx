ecf721e19e9c03e95eca15e7dcdd4c831c69b934
ryhhill1998 <ryhhill1998@outlook.com>
2023-04-23 15:35:17 +0100

    Added add friend popup to app

--- src/routes/FriendsPage/FriendsPage.jsx
+++ src/routes/FriendsPage/FriendsPage.jsx
@@ -5,15 +5,27 @@ import {useNavigate} from "react-router-dom";
 import UserIcon from "../../common/components/UserIcon/UserIcon";
 import SearchBox from "../../common/components/SearchBox/SearchBox";
 import {useState} from "react";
+import FormField from "../../common/components/FormField/FormField";
 
 const FriendsPage = () => {
 
     const navigate = useNavigate();
 
     const [searchIsVisible, setSearchIsVisible] = useState(false);
+    const [addPopupIsVisible, setAddPopupIsVisible] = useState(false);
+    const [addFriendId, setAddFriendId] = useState("");
 
     const handleBackClick = () => {
         navigate("/profile");
+    };
+
+    const handleYesClick = () => {
+        console.log(addFriendId);
+    };
+
+    const handleNoClick = () => {
+        setAddPopupIsVisible(false);
+        setAddFriendId("");
     };
 
     return (
@@ -42,7 +54,7 @@ const FriendsPage = () => {
 
             <main className="container">
                 <div className="links-container">
-                    <button>
+                    <button onClick={() => setAddPopupIsVisible(true)}>
                         Add
                         <FontAwesomeIcon className="icon" icon={faPlus}/>
                     </button>
@@ -52,6 +64,27 @@ const FriendsPage = () => {
                         <FontAwesomeIcon className="icon" icon={faLink}/>
                     </button>
                 </div>
+
+                {addPopupIsVisible && (
+                    <div className="confirm-checkin-popup">
+                        <div className="date-container">
+                            <FormField
+                                label="Friend ID"
+                                name="visitDate"
+                                type="text"
+                                value={addFriendId}
+                                onChangeHandler={({target}) => setAddFriendId(target.value)}
+                            />
+                        </div>
+
+                        <p>Send friend request?</p>
+
+                        <div className="buttons-container">
+                            <button onClick={handleYesClick}>Yes</button>
+                            <button onClick={handleNoClick}>No</button>
+                        </div>
+                    </div>
+                )}
 
                 <div className="friend-icons-container">
                     {[...Array(9)].map(entry => (

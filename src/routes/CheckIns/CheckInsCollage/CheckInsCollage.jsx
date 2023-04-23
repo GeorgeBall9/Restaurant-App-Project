bdd7a0b19f58ebe228d097823b1b6ae18d859619
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-04-23 15:49:14 +0100

    Photo collage styling

--- src/routes/CheckIns/CheckInsCollage/CheckInsCollage.jsx
+++ src/routes/CheckIns/CheckInsCollage/CheckInsCollage.jsx
@@ -1,11 +1,28 @@
 import "./CheckInsCollage.css";
 
+import CustomCollage from "./CustomCollage/CustomCollage.jsx";
 import { useState, useEffect } from "react";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const CheckInsCollage = ({ restaurant, onClose }) => {
     const [isVisible, setIsVisible] = useState(false);
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const demoPhotos = [
+        { src: "https://picsum.photos/id/1011/600/400", alt: "Photo 1" },
+        { src: "https://picsum.photos/id/1012/600/400", alt: "Photo 2" },
+        { src: "https://picsum.photos/id/1013/600/400", alt: "Photo 3" },
+        { src: "https://picsum.photos/id/1014/600/400", alt: "Photo 4" },
+        { src: "https://picsum.photos/id/1015/600/400", alt: "Photo 5" },
+        { src: "https://picsum.photos/id/1016/600/400", alt: "Photo 6" },
+        { src: "https://picsum.photos/id/1010/600/400", alt: "Photo 7" },
+        { src: "https://picsum.photos/id/1018/600/400", alt: "Photo 8" },
+        { src: "https://picsum.photos/id/1019/600/400", alt: "Photo 9" },
+        { src: "https://picsum.photos/id/1020/600/400", alt: "Photo 10" },
+        { src: "https://picsum.photos/id/1021/600/400", alt: "Photo 11" },
+        { src: "https://picsum.photos/id/1022/600/400", alt: "Photo 11" },
+    ];
 
     const handleBackClick = () => {
         setIsVisible(false);
@@ -14,29 +31,33 @@ const CheckInsCollage = ({ restaurant, onClose }) => {
         }, 300);
     };
 
+    const handleExpand = () => {
+        setIsExpanded(true);
+    };
+
     useEffect(() => {
         setIsVisible(true);
     }, []);
-    
-    return (
-        <div className={`collage-popup ${isVisible ? "visible" : ""}`}>
-            <header>
-                <button onClick={handleBackClick}>
-                    <FontAwesomeIcon className="icon" icon={faArrowLeft} />
-                    Back
-                </button>
-                <h2>{restaurant.name}</h2>
-            </header>
-            {/* Render the collage of photos here */}
-            <div className="collage-popup-photos">
-                <img src={restaurant.photoUrl} alt={restaurant.name} />
 
-            </div>
-            <div className="collage-popup-function">
-                <button>Add Photo</button>
-            </div>
+    return (
+        <div className={`collage-popup ${isVisible ? "visible" : ""} ${isExpanded ? "expanded" : ""}`}>
+          <header>
+            <button onClick={handleBackClick}>
+              <FontAwesomeIcon className="icon" icon={faArrowLeft} />
+              Back
+            </button>
+            <h2>{restaurant.name}</h2>
+          </header>
+          {/* Render the collage of photos using CustomCollage */}
+          <div className={`collage-popup-photos ${isExpanded ? "collage-container-expanded" : ""}`}>
+            <CustomCollage images={demoPhotos} rows={isExpanded ? 4 : 2} columns={isExpanded ? 4 : 2} onExpand={handleExpand} />
+          </div>
+    
+          <div className="collage-popup-function">
+            <button>Add Photo</button>
+          </div>
         </div>
-    );
-};
+      );
+    };
 
 export default CheckInsCollage;
---
+++ src/routes/CheckIns/CheckInsCollage/CustomCollage/CustomCollage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Masonry from "react-masonry-css";
+import "./CustomCollage.css";
+
+const CustomCollage = ({ images, rows, columns, onExpand }) => {
+    const imageElements = images.slice(0, rows * columns - 1).map((image, index) => (
+        <img key={index} src={image.src} alt={image.alt} className="collage-image" />
+    ));
+
+    const showMore = images.length > rows * columns;
+    const remainingImages = showMore ? images.length - rows * columns + 1 : 0;
+
+    return (
+        <div
+            className="collage-container"
+            style={{ gridTemplateRows: `repeat(${rows}, 1fr)`, gridTemplateColumns: `repeat(${columns}, 1fr)` }}
+        >
+            {imageElements}
+            {showMore && (
+                <div className="collage-image-wrapper">
+                    <img src={images[rows * columns - 1].src} alt={images[rows * columns - 1].alt} className="collage-image" />
+                    <div className="collage-image-overlay" onClick={onExpand}>
+                        Show more +{remainingImages}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default CustomCollage;

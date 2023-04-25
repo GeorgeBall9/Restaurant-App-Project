a7e8163c8cf4459b9b9557395c99ede59112b645
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-04-25 18:04:07 +0100

    FriendsProfile TODO

--- src/routes/FriendsPage/FriendsProfile/FriendsProfile.jsx
+++ src/routes/FriendsPage/FriendsProfile/FriendsProfile.jsx
@@ -1,11 +1,11 @@
 import './FriendsProfile.css';
 
-import FriendInfo from '../FriendCard/FriendInfo/FriendInfo';
-import {Link, useNavigate} from "react-router-dom";
+import { getUserFromUserId } from '../../../firebase/firebase';
+import { Link, useNavigate } from "react-router-dom";
 import UserIcon from '../../../common/components/UserIcon/UserIcon';
-import {useSelector} from "react-redux";
-import { selectFriends ,selectUserId, selectDisplayName, selectIconColour } from '../../../features/user/userSlice';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { useSelector } from "react-redux";
+import { selectFriends, selectUserId, selectDisplayName, selectIconColour } from '../../../features/user/userSlice';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faArrowLeft,
     faBookmark,
@@ -16,7 +16,7 @@ import {
     faShareNodes,
     faUser, faUserGroup
 } from "@fortawesome/free-solid-svg-icons";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import ContributionsButton from '../../ProfilePage/ContributionsButton/ContributionsButton';
 
 const FriendsProfile = () => {
@@ -28,12 +28,16 @@ const FriendsProfile = () => {
     const iconColour = useSelector(selectIconColour);
 
     const [idCopied, setIdCopied] = useState(false);
+    const [friendProfile, setFriendProfile] = useState("");
 
     useEffect(() => {
-        if (!userId) {
-            navigate("/sign-in")
-        }
-    }, []);
+        const fetchFriendProfile = async () => {
+            const user = await getUserFromUserId(userId);
+            setFriendProfile(user);
+        };
+
+        fetchFriendProfile();
+    }, [userId]);
 
     const handleBackClick = () => {
         navigate("/friends");
@@ -44,19 +48,20 @@ const FriendsProfile = () => {
             .then(() => setIdCopied(true));
     };
 
+
     return (
         <div className="friends-profile-page-container">
             <header>
                 <div className="container">
                     <button onClick={handleBackClick}>
-                        <FontAwesomeIcon className="icon" icon={faArrowLeft}/>
+                        <FontAwesomeIcon className="icon" icon={faArrowLeft} />
                         Back
                     </button>
 
                     <h1>{displayName}'s Profile</h1>
 
-                    <button  style={{visibility: "hidden"}}>
-                        <FontAwesomeIcon className="icon" icon={faArrowLeft}/>
+                    <button style={{ visibility: "hidden" }}>
+                        <FontAwesomeIcon className="icon" icon={faArrowLeft} />
                         Back
                     </button>
                 </div>
@@ -65,21 +70,21 @@ const FriendsProfile = () => {
             <main className="container">
                 <section className="friends-profile-info-container">
                     <div className="user-icon-container">
-                        <UserIcon size="xLarge" colour={iconColour} skeleton={!iconColour}/>
+                        <UserIcon size="xLarge" colour={iconColour} skeleton={!iconColour} />
                     </div>
 
-                    <p style={{visibility: displayName ? "visible" : "hidden"}}>{displayName || "display name"}</p>
+                    <p style={{ visibility: displayName ? "visible" : "hidden" }}>{displayName || "display name"}</p>
 
                     <button className="copy-id-button" onClick={handleCopyIdClick}>
-                        {idCopied ? "Copied" :"Copy user ID"}
-                        <FontAwesomeIcon className="icon" icon={idCopied ? faCircleCheck : faCopy}/>
+                        {idCopied ? "Copied" : "Copy user ID"}
+                        <FontAwesomeIcon className="icon" icon={idCopied ? faCircleCheck : faCopy} />
                     </button>
                 </section>
 
                 <section className="contributions-container">
-                    <ContributionsButton route="/view-check-ins/:userId" icon={faCircleCheck} name="Check ins"/>
+                    <ContributionsButton route="/view-check-ins/:userId" icon={faCircleCheck} name="Check ins" />
 
-                    <ContributionsButton route="/view-reviews/:userId" icon={faComment} name="Reviews"/>
+                    <ContributionsButton route="/view-reviews/:userId" icon={faComment} name="Reviews" />
                 </section>
             </main>
         </div>

608aac411de1a8b49a8187b01595384a5142d81d
ryhhill1998 <ryhhill1998@outlook.com>
2023-04-27 14:41:27 +0100

    Added buttons to select icon type when changing user icon in edit profile page

--- src/features/changeIconPopup/ChangeIconPopup/ChangeIconPopup.jsx
+++ src/features/changeIconPopup/ChangeIconPopup/ChangeIconPopup.jsx
@@ -6,6 +6,8 @@ import {hideChangeIconPopup} from "../changeIconPopupSlice";
 import UserIconButton from "./UserIconButton/UserIconButton";
 import {updateUserIconColour} from "../../../firebase/firebase";
 import {hideOverlay} from "../../overlay/overlaySlice";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUser, faImage} from "@fortawesome/free-regular-svg-icons";
 
 const colours = ["#FF2E63", "#B3E5BE", "#AA77FF", "#19A7CE", "#FE6244", "#FFDD83", "#E6A4B4", "#5D9C59", "#E21818"];
 
@@ -17,6 +19,8 @@ const ChangeIconPopup = () => {
     const iconColour = useSelector(selectIconColour);
 
     const [iconButtons, setIconButtons] = useState(colours.map(colour => ({colour, selected: false})));
+    const [iconType, setIconType] = useState("");
+    const [headerText, setHeaderText] = useState("icon type");
 
     useEffect(() => {
         if (!iconColour) return;
@@ -77,21 +81,55 @@ const ChangeIconPopup = () => {
                 <button onClick={handleResetPopupClick}>Reset</button>
             </div>
 
-            <h2>Choose an icon</h2>
+            <h2>Select an {headerText}</h2>
 
-            <div className="icons-container">
-                {iconButtons.map((button, i) => (
-                    <UserIconButton
-                        key={i}
-                        colour={button.colour}
-                        index={i}
-                        selected={button.selected}
-                        handleClick={handleIconButtonClick}
-                    />
-                ))}
+            {iconType === "avatar" && (
+                <div className="icons-container">
+                    {iconButtons.map((button, i) => (
+                        <UserIconButton
+                            key={i}
+                            colour={button.colour}
+                            index={i}
+                            selected={button.selected}
+                            handleClick={handleIconButtonClick}
+                        />
+                    ))}
+                </div>
+            )}
+
+            {iconType && (
+                <button className="apply-button" onClick={handleSavePopupClick}>Save</button>
+            )}
+
+            <div className="buttons-container">
+                <button>
+                    <div style={{visibility: "hidden"}}><FontAwesomeIcon className="icon" icon={faUser}/></div>
+                    Avatar
+                    <div><FontAwesomeIcon className="icon" icon={faUser}/></div>
+                </button>
+
+                <button>
+                    <div style={{visibility: "hidden"}}><FontAwesomeIcon className="icon" icon={faImage}/></div>
+                    Image
+                    <div><FontAwesomeIcon className="icon" icon={faImage}/></div>
+                </button>
             </div>
 
-            <button className="apply-button" onClick={handleSavePopupClick}>Save</button>
+            {/*<h2>Choose an icon</h2>*/}
+
+            {/*<div className="icons-container">*/}
+            {/*    {iconButtons.map((button, i) => (*/}
+            {/*        <UserIconButton*/}
+            {/*            key={i}*/}
+            {/*            colour={button.colour}*/}
+            {/*            index={i}*/}
+            {/*            selected={button.selected}*/}
+            {/*            handleClick={handleIconButtonClick}*/}
+            {/*        />*/}
+            {/*    ))}*/}
+            {/*</div>*/}
+
+            {/*<button className="apply-button" onClick={handleSavePopupClick}>Save</button>*/}
         </div>
     );
 };

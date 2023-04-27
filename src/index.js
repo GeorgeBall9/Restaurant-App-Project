6b04807866813a1c93e418d6f885724259964b2b
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-04-27 10:07:27 +0100

    Added FriendsCheckIns component to route

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ import PreviewReviews from "./routes/PreviewReviews/PreviewReviews";
 import ReviewsPage from "./routes/ReviewsPage/ReviewsPage";
 import FriendsPage from "./routes/FriendsPage/FriendsPage";
 import FriendsProfile from "./routes/FriendsPage/FriendsProfile/FriendsProfile";
+import FriendsCheckIns from "./routes/FriendsPage/FriendsProfile/FriendsCheckIns/FriendsCheckIns";
 
 const router = createBrowserRouter([
     {
@@ -92,7 +93,7 @@ const router = createBrowserRouter([
                     },
                     {
                         path: "view-check-ins/:userId",
-                        element: <CheckIns/>,
+                        element: <FriendsCheckIns/>,
                     },
                     {
                         path: "view-reviews/:userId",

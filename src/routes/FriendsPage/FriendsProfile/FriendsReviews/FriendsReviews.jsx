61382933695b30ef51b42c058f52fbc145e29bf2
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-05-03 19:39:04 +0100

    NoResults rendering

--- src/routes/FriendsPage/FriendsProfile/FriendsReviews/FriendsReviews.jsx
+++ src/routes/FriendsPage/FriendsProfile/FriendsReviews/FriendsReviews.jsx
@@ -1,15 +1,16 @@
 import "../../../PreviewReviews/PreviewReviews.css";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faExpand} from "@fortawesome/free-solid-svg-icons";
-import {useNavigate} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect, useState} from "react";
-import {getReviewsByUserId} from "../../../../firebase/firebase";
-import {selectReview} from "../../../../features/reviews/reviewsSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExpand } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { getReviewsByUserId } from "../../../../firebase/firebase";
+import { selectReview } from "../../../../features/reviews/reviewsSlice";
 import RestaurantImage from "../../../../common/components/RestaurantImage/RestaurantImage";
 import StarRating from "../../../../common/components/StarRating/StarRating";
-import {selectDisplayedFriend} from "../../../../features/user/userSlice";
+import { selectDisplayedFriend } from "../../../../features/user/userSlice";
 import ProfileNavigation from "../../../../common/components/ProfileNavigation/ProfileNavigation";
+import NoResults from "../../../../common/components/NoResults/NoResults";
 
 const FriendsReviews = () => {
 
@@ -37,31 +38,34 @@ const FriendsReviews = () => {
         <div className="preview-reviews-container">
             {displayedFriend && (
                 <>
-                    <ProfileNavigation pageTitle={`${displayedFriend.displayName}'s Reviews`}/>
+                    <ProfileNavigation pageTitle={`${displayedFriend.displayName}'s Reviews`} />
 
                     <main className="reviews-container container">
-                        {friendReviews && friendReviews
-                            .map(({id, rating, title, content, restaurantId, restaurantName, photoUrl}) => (
-                            <div key={id} className="review-card">
-                                <div className="container-lhs">
-                                    <RestaurantImage photoUrl={photoUrl} name={restaurantName}/>
+                        {friendReviews && friendReviews.length === 0 ? (
+                            <NoResults mainText="No reviews found." subText={`${displayedFriend.displayName} hasn't written any reviews yet.`} />
+                        ) : (
+                            friendReviews && friendReviews.map(({ id, rating, title, content, restaurantId, restaurantName, photoUrl }) => (
+                                <div key={id} className="review-card">
+                                    <div className="container-lhs">
+                                        <RestaurantImage photoUrl={photoUrl} name={restaurantName} />
 
-                                    <div className="preview">
-                                        <h3 style={{margin: 0}}>{title}</h3>
+                                        <div className="preview">
+                                            <h3 style={{ margin: 0 }}>{title}</h3>
 
-                                        <StarRating rating={rating}/>
+                                            <StarRating rating={rating} />
 
-                                        <p>{content}</p>
+                                            <p>{content}</p>
+                                        </div>
+                                    </div>
+
+                                    <div className="container-rhs">
+                                        <button onClick={() => handleExpandClick(id, restaurantId)}>
+                                            <FontAwesomeIcon icon={faExpand} className="icon" />
+                                        </button>
                                     </div>
                                 </div>
-
-                                <div className="container-rhs">
-                                    <button onClick={() => handleExpandClick(id, restaurantId)}>
-                                        <FontAwesomeIcon icon={faExpand} className="icon"/>
-                                    </button>
-                                </div>
-                            </div>
-                        ))}
+                            ))
+                        )}
                     </main>
                 </>
             )}

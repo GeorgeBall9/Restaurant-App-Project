5b3bc91bbfb793d3851974de8f3c6e50dd5f04e8
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-05-04 13:01:27 +0100

    Card styling

--- src/routes/CheckIns/CheckInsCollage/CheckInsCard/CheckInsCard.jsx
+++ src/routes/CheckIns/CheckInsCollage/CheckInsCard/CheckInsCard.jsx
@@ -2,19 +2,27 @@ import React from "react";
 import "./CheckInsCard.css";
 
 const CheckInsCard = ({ restaurant, date, userIcon, friendsIcons }) => {
-    return (
-        <div className="check-ins-card">
-            <div className="user-icons">
-                <img src={userIcon} alt="User Icon" />
-                {friendsIcons && friendsIcons.map((icon, index) => (
-                    <img key={index} src={icon} alt={`Friend ${index + 1}`} />
-                ))}
-            </div>
-            <h3>{restaurant}</h3>
-            <p>{date}</p>
-            
+  const formattedDate = new Date(date).toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+
+  return (
+    <div className="check-ins-card">
+      <div className="card-header">
+        <div className="user-icons">
+          <img src={userIcon} alt="User Icon" />
+          {friendsIcons &&
+            friendsIcons.map((icon, index) => (
+              <img key={index} src={icon} alt={`Friend ${index + 1}`} />
+            ))}
         </div>
-    );
+        <p className="visit-date">Visited: {formattedDate}</p>
+      </div>
+      <h3 className="restaurant-name">{restaurant}</h3>
+    </div>
+  );
 };
 
 export default CheckInsCard;

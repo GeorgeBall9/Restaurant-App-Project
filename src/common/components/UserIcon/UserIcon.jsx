0eba80c236befdc0138d5bc8be653c344ae74be2
ryhhill1998 <ryhhill1998@outlook.com>
2023-05-05 12:33:19 +0100

    fixed appearance of user icons

--- src/common/components/UserIcon/UserIcon.jsx
+++ src/common/components/UserIcon/UserIcon.jsx
@@ -3,22 +3,22 @@ import {faUser} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {useState} from "react";
 
-const sizesMap = {
-    small: "30px",
-    medium: "50px",
-    large: "60px",
-    larger: "70px",
-    xLarge: "100px",
+const stylesMap = {
+    small: {height: "30px", width: "30px", borderWidth: "2px"},
+    medium: {height: "50px", width: "50px", borderWidth: "2px"},
+    large: {height: "60px", width: "60px", borderWidth: "3px"},
+    larger: {height: "70px", width: "70px", borderWidth: "4px"},
+    xLarge: {height: "100px", width: "100px", borderWidth: "5px"},
 };
 
 const UserIcon = ({size, imageUrl = null}) => {
 
     const [imageIsLoaded, setImageIsLoaded] = useState(false);
 
-    const dimensions = sizesMap[size];
+    const style = stylesMap[size];
 
     return (
-        <div className="user-icon" style={{height: dimensions, width: dimensions}}>
+        <div className="user-icon" style={style}>
             {imageUrl ? (
                 <img
                     src={imageUrl}

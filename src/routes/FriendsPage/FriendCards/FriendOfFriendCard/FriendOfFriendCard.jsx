0b44818d3768ba326f07a70d92944c684d05fea7
ryhhill1998 <ryhhill1998@outlook.com>
2023-05-07 15:41:45 +0100

    added confirmations popups to all requests/interactions on friends page

--- src/routes/FriendsPage/FriendCards/FriendOfFriendCard/FriendOfFriendCard.jsx
+++ src/routes/FriendsPage/FriendCards/FriendOfFriendCard/FriendOfFriendCard.jsx
@@ -1,15 +1,30 @@
 import FriendCard from "../FriendCard/FriendCard";
+import ConfirmationPopupView from "../../../../common/components/ConfirmationPopupView/ConfirmationPopupView";
+import {useState} from "react";
 
 const FriendOfFriendCard = ({displayName, iconColour, profilePhotoUrl, mutualFriends, handleAddClick}) => {
+
+    const [confirmAddPopupIsVisible, setConfirmAddPopupIsVisible] = useState(false);
+
     return (
-        <FriendCard
-            displayName={displayName}
-            iconColour={iconColour}
-            profilePhotoUrl={profilePhotoUrl}
-            mutualFriends={mutualFriends}
-            status="friendOfFriend"
-            handleAddClick={handleAddClick}
-        />
+        <>
+            <FriendCard
+                displayName={displayName}
+                iconColour={iconColour}
+                profilePhotoUrl={profilePhotoUrl}
+                mutualFriends={mutualFriends}
+                status="friendOfFriend"
+                handleAddClick={() => setConfirmAddPopupIsVisible(true)}
+            />
+
+            {confirmAddPopupIsVisible && (
+                <ConfirmationPopupView
+                    message={`Add ${displayName} as a friend?`}
+                    handleYesClick={handleAddClick}
+                    handleNoClick={() => setConfirmAddPopupIsVisible(false)}
+                />
+            )}
+        </>
     );
 };
 
--- src/routes/FriendsPage/FriendCards/PendingFriendCard/PendingFriendCard.jsx
+++ src/routes/FriendsPage/FriendCards/PendingFriendCard/PendingFriendCard.jsx
@@ -2,6 +2,8 @@ import FriendCard from "../FriendCard/FriendCard";
 import {cancelFriendRequest} from "../../../../firebase/firebase";
 import {selectUserId, setFriends} from "../../../../features/user/userSlice";
 import {useDispatch, useSelector} from "react-redux";
+import ConfirmationPopupView from "../../../../common/components/ConfirmationPopupView/ConfirmationPopupView";
+import {useState} from "react";
 
 const PendingFriendCard = ({id, displayName, iconColour, profilePhotoUrl, mutualFriends}) => {
 
@@ -9,20 +11,32 @@ const PendingFriendCard = ({id, displayName, iconColour, profilePhotoUrl, mutual
 
     const userId = useSelector(selectUserId);
 
+    const [confirmACancelPopupIsVisible, setConfirmCancelPopupIsVisible] = useState(false);
+
     const handleCancelClick = async () => {
         const updatedFriends = await cancelFriendRequest(userId, id);
         dispatch(setFriends(updatedFriends));
     };
 
     return (
-        <FriendCard
-            displayName={displayName}
-            iconColour={iconColour}
-            profilePhotoUrl={profilePhotoUrl}
-            mutualFriends={mutualFriends}
-            status="pending"
-            handleCancelClick={handleCancelClick}
-        />
+        <>
+            <FriendCard
+                displayName={displayName}
+                iconColour={iconColour}
+                profilePhotoUrl={profilePhotoUrl}
+                mutualFriends={mutualFriends}
+                status="pending"
+                handleCancelClick={() => setConfirmCancelPopupIsVisible(true)}
+            />
+
+            {confirmACancelPopupIsVisible && (
+                <ConfirmationPopupView
+                    message={`Cancel friend request to ${displayName}?`}
+                    handleYesClick={handleCancelClick}
+                    handleNoClick={() => setConfirmCancelPopupIsVisible(false)}
+                />
+            )}
+        </>
     );
 };
 

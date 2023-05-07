aed832f860d5e02a8c80ef49655c76b19ed1ab7e
ryhhill1998 <ryhhill1998@outlook.com>
2023-05-07 18:44:46 +0100

    added popup to indicate no results

--- src/routes/MapPage/MapPage.jsx
+++ src/routes/MapPage/MapPage.jsx
@@ -13,25 +13,55 @@ import Slider from "./Slider/Slider";
 import MapView from "../../common/components/MapView/MapView";
 import {useEffect, useState} from "react";
 import {useSelector} from "react-redux";
-import {selectRestaurants} from "../../features/restaurants/restaurantsSlice";
+import {selectRestaurants, selectRestaurantsFetchStatus} from "../../features/restaurants/restaurantsSlice";
 import {selectUserPosition} from "../../features/location/locationSlice";
+import PrimaryButton from "../../common/components/ButtonViews/PrimaryButton/PrimaryButton";
+import NoResults from "../../common/components/NoResults/NoResults";
 
 const MapPage = () => {
 
     const restaurants = useSelector(selectRestaurants);
+    const restaurantsFetchStatus = useSelector(selectRestaurantsFetchStatus);
     const userPosition = useSelector(selectUserPosition);
 
     const [windowHeight, setWindowHeight] = useState(+window.innerHeight);
+    const [errorPopupIsVisible, setErrorPopupIsVisible] = useState(false);
+    const [mapIsLoaded, setMapIsLoaded] = useState(false);
 
     useEffect(() => {
         setWindowHeight(+window.innerHeight)
     }, [window.innerHeight]);
 
+    useEffect(() => {
+        if (mapIsLoaded && restaurantsFetchStatus === "idle" && !restaurants?.length) {
+            setErrorPopupIsVisible(true);
+        } else {
+            setErrorPopupIsVisible(false);
+        }
+    }, [restaurants, restaurantsFetchStatus, mapIsLoaded]);
+
     return (
         <div className="map-page-container">
             <Navigation view="map"/>
 
-            <MapView centrePosition={userPosition} zoom={14} height={windowHeight} restaurants={restaurants}/>
+            <MapView
+                centrePosition={userPosition}
+                zoom={14}
+                height={windowHeight}
+                restaurants={restaurants}
+                handleLoad={() => setMapIsLoaded(true)}
+            />
+
+            {errorPopupIsVisible && (
+                <div className="location-error-popup">
+                    <NoResults
+                        mainText="No open restaurants nearby!!"
+                        subText="Try entering a different search location or check back later"
+                    />
+
+                    <PrimaryButton text="Close" handleClick={() => setErrorPopupIsVisible(false)}/>
+                </div>
+            )}
 
             <Slider/>
         </div>

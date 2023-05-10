1cc9c5a6a722b75ea1f34121c54a1597f00e68f1
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-05-10 14:05:02 +0100

    comments

--- src/routes/CheckIns/CheckInsMapChildren/CheckInsMapChildren.jsx
+++ src/routes/CheckIns/CheckInsMapChildren/CheckInsMapChildren.jsx
@@ -1,24 +1,35 @@
+/*
+ Description: CheckInsMapChildren component which renders the CheckInMarker on a page
+ Author: Ryan Henzell-Hill
+ Contact: ryan.henzell-hill@outlook.com
+ */
+
+// stylesheet
 import "./CheckInsMapChildren.css";
+// Import required dependencies and components
 import {FullscreenControl} from "react-map-gl";
 import {Popup} from "react-map-gl";
 import RestaurantMarker from "../../../common/components/map/RestaurantMarker/RestaurantMarker";
 
+// Define the 'CheckInsMapChildren' component
 const CheckInsMapChildren = ({checkIns, displayedRestaurant}) => {
 
     return (
-        <>
+        <>  
+            {/* Iterate through checkIns and render RestaurantMarker for each checkIn */}
             {checkIns && checkIns.map(({id, restaurant}) => {
                 const {id: restaurantId, longitude, latitude, name} = restaurant;
 
                 return (
                     <div key={id}>
+                        {/* Render RestaurantMarker component */}
                         <RestaurantMarker
                             restaurant={{...restaurant, checkInId: id}}
                             visible={true}
                             type="check-in"
                             selected={displayedRestaurant.id === restaurantId}
                         />
-
+                        {/* Display Popup component for the displayedRestaurant */}
                         {displayedRestaurant?.id === restaurantId && (
                             <Popup
                                 longitude={longitude}
@@ -37,7 +48,7 @@ const CheckInsMapChildren = ({checkIns, displayedRestaurant}) => {
                     </div>
                 );
             })}
-
+            {/* Render FullscreenControl component for the map */}
             <FullscreenControl position="bottom-right"/>
         </>
     );

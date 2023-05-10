e09af2c770f5a31611671f34b3786e3376fad5ff
ryhhill1998 <ryhhill1998@outlook.com>
2023-05-10 15:53:53 +0100

    commented restaurant marker

--- src/common/components/map/RestaurantMarker/RestaurantMarker.jsx
+++ src/common/components/map/RestaurantMarker/RestaurantMarker.jsx
@@ -1,16 +1,31 @@
+/*
+Description: Restaurant marker component to appear on maps
+Author: Ryan Henzell-Hill
+Contact: ryan.henzell-hill@outlook.com
+*/
+
+// stylesheet
 import "./RestaurantMarker.css";
+
+// mapbox dependencies
 import {Marker} from "react-map-gl";
+
+// redux imports
 import {displayRestaurant} from "../../../../features/map/mapSlice";
 import {setActiveSlide} from "../../../../features/slider/sliderSlice";
 import {useDispatch} from "react-redux";
 import {setSelectedCheckInsById} from "../../../../features/checkIns/checkInsSlice";
 
+// RestaurantMarker component
 const RestaurantMarker = ({restaurant, index, selected, visible, type = "main"}) => {
 
+    // deconstruct properties of restaurant prop
     const {id, name, longitude, latitude, photoUrl} = restaurant;
 
+    // initialise dispatch
     const dispatch = useDispatch();
 
+    // adjust marker visibility depending on visible prop
     const style = {
         visibility: visible ? "visible" : "hidden",
         zIndex: selected ? 10 : 0,

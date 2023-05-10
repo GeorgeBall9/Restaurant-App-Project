000ed926659081a287f3051d2dc0c2584cfdf90c
ryhhill1998 <ryhhill1998@outlook.com>
2023-05-10 09:33:22 +0100

    added comments to primary button component

--- src/common/components/buttons/PrimaryButton/PrimaryButton.jsx
+++ src/common/components/buttons/PrimaryButton/PrimaryButton.jsx
@@ -1,8 +1,19 @@
+/*
+Description: Generic button component for use throughout application
+Author: Ryan Henzell-Hill
+Contact: ryan.henzell-hill@outlook.com
+*/
+
+// stylesheet
 import "./PrimaryButton.css";
+
+// import generic button component
 import Button from "../Button/Button";
 
+// PrimaryButton component
 const PrimaryButton = ({handleClick, children, text, icon, size, active, type, width, margin}) => {
 
+    // Render the generic Button component with specific props
     return (
         <Button
             className="primary"

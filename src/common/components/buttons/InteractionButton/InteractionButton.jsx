5bfa82052d0a5478514803acf4435a525f7dd9b2
ryhhill1998 <ryhhill1998@outlook.com>
2023-05-10 09:37:16 +0100

    added comments to interaction button

--- src/common/components/buttons/InteractionButton/InteractionButton.jsx
+++ src/common/components/buttons/InteractionButton/InteractionButton.jsx
@@ -1,9 +1,20 @@
+/*
+Description: Primary button component built off generic Button component for use throughout application
+Author: Ryan Henzell-Hill
+Contact: ryan.henzell-hill@outlook.com
+*/
+
+// stylesheet
 import "./InteractionButton.css";
+
+// fontawesome import
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
+// InteractionButton component
 const InteractionButton = ({icon, solidIcon, isSolid, handleClick, style}) => {
     return (
         <button className="interaction-button" onClick={handleClick}>
+            {/* Conditionally render the FontAwesomeIcon based on the isSolid prop */}
             {isSolid ? (
                 <FontAwesomeIcon icon={solidIcon} className="icon" style={style}/>
             ) : (

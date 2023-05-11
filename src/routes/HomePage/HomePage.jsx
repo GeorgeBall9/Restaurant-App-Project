ba8389533bf2252e1ce2ccba195c5dc2abd0bc9f
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-05-11 14:11:05 +0100

    comment

--- src/routes/HomePage/HomePage.jsx
+++ src/routes/HomePage/HomePage.jsx
@@ -1,4 +1,11 @@
+/*
+ Description: Home page component.
+ Author: Ryan Henzell-Hill
+ Contact: ryan.henzell-hill@outlook.com
+ */
+// stylesheet
 import "./HomePage.css";
+// Import dependencies
 import Navigation from "../../common/components/navigations/Navigation/Navigation";
 import NoResults from "../../common/components/NoResults/NoResults";
 import { useDispatch, useSelector } from "react-redux";
@@ -23,6 +30,7 @@ const HomePage = () => {
     const [restaurantsUpdatedByDB, setRestaurantsUpdatedByDB] = useState(false);
     const [navHeight, setNavHeight] = useState(null);
 
+    // Show spinner when fetching data
     useEffect(() => {
         if (fetchStatus === "pending") {
             dispatch(showSpinner());
@@ -31,11 +39,13 @@ const HomePage = () => {
         }
     }, [fetchStatus]);
 
+    // Fetch restaurant data from DB
     const fetchRestaurantDataFromDB = async () => {
         return Promise.all(restaurants
             .map(async (restaurant) => await getRestaurantById(restaurant.id) || restaurant));
     };
 
+    // Update restaurants in state with data from DB
     useEffect(() => {
         if (restaurantsUpdatedByDB || !restaurants) return;
 
@@ -46,10 +56,12 @@ const HomePage = () => {
             });
     }, [restaurants]);
 
+    // Check if restaurant is highly recommended
     const checkHighlyRecommended = (restaurant) => {
         return restaurant.recommendations >= 10;
     };
 
+    // Set margin top of cards container to nav height
     useEffect(() => {
         if (!cardsContainerRef || !navHeight) return;
 

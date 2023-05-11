4384743daf03affad238c3d54f7e38816facf4b1
ryhhill1998 <ryhhill1998@outlook.com>
2023-05-11 17:10:20 +0100

    commented review form

--- src/common/components/reviews/ReviewForm/ReviewForm.jsx
+++ src/common/components/reviews/ReviewForm/ReviewForm.jsx
@@ -1,20 +1,38 @@
+/*
+Description: Review form for use in reviews displays
+Author: George Ball
+Contact: georgeball14@hotmail.com
+*/
+
+// stylesheet
 import './ReviewForm.css';
-import {forwardRef, useEffect, useRef, useState} from 'react';
+
+// react imports
+import {forwardRef, useState} from 'react';
+
+// firebase imports
 import {
     addRestaurantReview,
     createNewRestaurantPhotoDoc,
     deleteRestaurantPhotoDoc,
     updateRestaurantReview
 } from "../../../../firebase/firebase";
+
+// redux imports
 import {useDispatch} from "react-redux";
 import {addReview, updateReview} from "../../../../features/reviews/reviewsSlice";
+
+// fontawesome imports
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPen} from "@fortawesome/free-solid-svg-icons";
+
+// component imports
 import UploadImagePopup from "../../popups/UploadImagePopup/UploadImagePopup";
 import InteractiveStarRating from "../../StarRating/IntearactiveStarRating/InteractiveStarRating";
 import InversePrimaryButton from "../../buttons/InversePrimaryButton/InversePrimaryButton";
 import FormField from "../../FormField/FormField";
 
+// default values for form fields
 const defaultFormFields = {
     rating: "",
     visitDate: new Date().toISOString().split("T")[0],
@@ -22,12 +40,16 @@ const defaultFormFields = {
     content: "",
 };
 
+// forward ref from parent component
 const ReviewForm = forwardRef((props, ref) => {
 
+    // deconstruct props
     const {restaurant, userId, edit, reviewId, reviewData, closeForm} = props;
 
+    // initialise dispatch
     const dispatch = useDispatch();
 
+    // state variables
     const [addImagesPopUpIsVisible, setAddImagesPopupIsVisible] = useState(false);
     const [isSubmitted, setIsSubmitted] = useState(false);
     const [errors, setErrors] = useState({});
@@ -35,11 +57,13 @@ const ReviewForm = forwardRef((props, ref) => {
     const [formData, setFormData] = useState(reviewData ? reviewData : defaultFormFields);
     const {rating, visitDate, title, content} = formData;
 
+    // handler function for upload photo button clicked
     const handleUploadPhotoClick = async (photoUrl, photoStoragePath) => {
         const photoId = await createNewRestaurantPhotoDoc(userId, restaurant.id, photoStoragePath);
         setUploadedPhotoId(photoId);
     };
 
+    // function to validate form - returns false if errors exist and sets specific errors to display feedback
     const validateForm = (rating, visitDate, title, content) => {
         const newErrors = {};
 
@@ -66,17 +90,23 @@ const ReviewForm = forwardRef((props, ref) => {
 
         setErrors(newErrors);
 
+        // check there are no errors
         return Object.keys(newErrors).length === 0;
     };
 
+    // handler function for form submission
     const handleSubmit = async (event) => {
+        // stop page reloading
         event.preventDefault();
 
+        // deconstruct formData
         const {rating, visitDate, title, content} = formData;
 
+        // check form is valid
         if (validateForm(rating, visitDate, title, content)) {
             const data = {rating: +rating, visitDate: +new Date(visitDate), title, content};
 
+            // check if form use type is edit or new review
             if (edit) {
                 const updatedData = {...data, photoId: uploadedPhotoId};
                 const updatedReview = await updateRestaurantReview(reviewId, updatedData);
@@ -89,30 +119,36 @@ const ReviewForm = forwardRef((props, ref) => {
 
             setIsSubmitted(true);
 
+            // close form after delay
             setTimeout(closeForm, 2000);
         }
     };
 
+    // handler function for a field in the form changing value
     const handleChange = ({target}) => {
         const {name, value} = target;
         setFormData({...formData, [name]: value});
     };
 
+    // handler function for star rating clicked
     const handleStarRatingClick = (value) => {
         handleChange({target: {name: 'rating', value}});
     };
 
+    // handler function for closing popup
     const handleClosePopupClick = () => {
         setAddImagesPopupIsVisible(false);
         document.querySelector(".file-upload-input").value = "";
     };
 
+    // handler function for clicking upload photo button
     const handlePhotoUpload = async (photoUrl, photoStoragePath) => {
         await handleUploadPhotoClick(photoUrl, photoStoragePath);
         document.querySelector(".file-upload-input").value = "";
         handleClosePopupClick();
     };
 
+    // handler function for removing uploaded photo
     const handleRemovePhoto = async () => {
         await deleteRestaurantPhotoDoc(uploadedPhotoId);
         setUploadedPhotoId(null);
@@ -121,6 +157,7 @@ const ReviewForm = forwardRef((props, ref) => {
     return (
         <div ref={ref} className="review-form">
             <form onSubmit={handleSubmit}>
+                {/* show edit mode if in edit mode */}
                 {edit && (
                     <h2 style={{margin: 0}}>
                         Editing
@@ -128,6 +165,7 @@ const ReviewForm = forwardRef((props, ref) => {
                     </h2>
                 )}
 
+                {/* show add image popup when add image button clicked */}
                 {addImagesPopUpIsVisible && (
                     <UploadImagePopup
                         handleCloseClick={handleClosePopupClick}
@@ -147,12 +185,14 @@ const ReviewForm = forwardRef((props, ref) => {
                         </label>
                     </div>
 
+                    {/* show uploaded photo feedback and remove button if photo has been uploaded */}
                     {uploadedPhotoId ? (
                         <div className="upload-feedback-container">
                             <p>1 image uploaded</p>
                             <button type="button" onClick={handleRemovePhoto}>Remove</button>
                         </div>
                     ) : (
+                        // show add photo button if no photo uploaded
                         <InversePrimaryButton
                             text="Add image"
                             type="button"
@@ -161,6 +201,7 @@ const ReviewForm = forwardRef((props, ref) => {
                     )}
                 </div>
 
+                {/* show errors if present */}
                 {errors.rating && <p className="rating-error error">{errors.rating}</p>}
 
                 <div>
@@ -206,6 +247,7 @@ const ReviewForm = forwardRef((props, ref) => {
                 )}
             </form>
 
+            {/* show feecback once form submitted */}
             {isSubmitted && (
                 <div className="confirmation-overlay">
                     <h3>Thank you for your review!</h3>

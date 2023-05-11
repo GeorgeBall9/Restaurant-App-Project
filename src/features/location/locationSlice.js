1ba0034a126c5a0548b954b48b8f6f5583e9c025
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-05-11 20:37:37 +0100

    locationSlice

--- src/features/location/locationSlice.js
+++ src/features/location/locationSlice.js
@@ -1,5 +1,7 @@
+// Definition of the location slice of the Redux store
 import {createSlice} from '@reduxjs/toolkit'
 
+// Define the initial state for the location slice
 const initialState = {
     userPosition: {
         latitude: localStorage.getItem("latitude") ?
@@ -18,22 +20,27 @@ const initialState = {
     usingCurrentLocation: false
 };
 
+// Define the location slice of the Redux store
 export const locationSlice = createSlice({
     name: 'location',
     initialState,
     reducers: {
+        // Update the user's position in the state
         updateUserPosition: (state, action) => {
             const {longitude, latitude} = action.payload;
             state.userPosition.longitude = longitude;
             state.userPosition.latitude = latitude;
         },
+        // Update the location description in the state
         setLocationDescription: (state, action) => {
             state.locationDescription = action.payload;
         },
+        // Set the location description in the state to the user's current location
         setUsingCurrentLocation: state => {
             state.usingCurrentLocation = true;
             state.locationDescription = "Current location";
         },
+        // Set the location description in the state to a custom location
         setUsingCustomLocation: state => {
             state.usingCurrentLocation = false;
         }
@@ -47,7 +54,10 @@ export const {
     setUsingCurrentLocation,
     setUsingCustomLocation,
 } = locationSlice.actions
+
+// Export selectors from the location slice
 export const selectUserPosition = state => state.location.userPosition;
 export const selectLocationDescription = state => state.location.locationDescription;
 export const selectUsingCurrentLocation = state => state.location.usingCurrentLocation;
+// Export the reducer from the location slice
 export default locationSlice.reducer

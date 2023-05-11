17061f653b48663da12a2aaef7e768464bb40d23
ryhhill1998 <ryhhill1998@outlook.com>
2023-05-11 18:16:23 +0100

    commented vote button

--- src/common/components/reviews/ReviewsList/ReviewCard/VoteButton/VoteButton.jsx
+++ src/common/components/reviews/ReviewsList/ReviewCard/VoteButton/VoteButton.jsx
@@ -1,6 +1,14 @@
-import "./VoteButton.css";
+/*
+Description: Vote button - present twice on each review card (one rotated 180 degrees as downvote)
+Author: Ryan Henzell-Hill
+Contact: ryan.henzell-hill@outlook.com
+*/
+
+// fontawesome imports
 import {faCircleUp as faSolidCircleUp} from "@fortawesome/free-solid-svg-icons";
 import {faCircleUp} from "@fortawesome/free-regular-svg-icons";
+
+// component imports
 import InteractionButton from "../../../../buttons/InteractionButton/InteractionButton";
 
 const VoteButton = ({isSolid, handleClick}) => {

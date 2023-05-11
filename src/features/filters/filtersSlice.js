d05f815d0c9d4d1d642854ee050abe90db14c1ab
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-05-11 20:18:19 +0100

    filtersSlice comments

--- src/features/filters/filtersSlice.js
+++ src/features/filters/filtersSlice.js
@@ -1,5 +1,7 @@
+// Import the necessary function from Redux Toolkit
 import {createSlice} from '@reduxjs/toolkit';
 
+// Define the initial state for the filters slice
 const initialState = {
     sortBy: null,
     cuisine: "Any",
@@ -7,26 +9,33 @@ const initialState = {
     appliedCuisineFilter: null
 };
 
+// Create and export the filters slice
 export const filtersSlice = createSlice({
     name: 'filters',
     initialState,
     reducers: {
+        // Update the sortBy filter in the state
         updateSortFilter: (state, action) => {
             state.sortBy = action.payload;
         },
+        // Reset the sortBy filter to its initial state
         resetSortFilter: state => {
             state.sortBy = null;
         },
+        // Update the cuisine filter in the state
         updateCuisineFilter: (state, action) => {
             state.cuisine = action.payload;
         },
+        // Reset the cuisine filter to its initial state
         resetCuisineFilter: state => {
             state.cuisine = "Any";
         },
+        // Apply the filters to the appliedSortByFilter and appliedCuisineFilter properties in the state
         applyFilters: state => {
             state.appliedSortByFilter = state.sortBy ? state.sortBy : null;
             state.appliedCuisineFilter = state.cuisine !== "Any" ? state.cuisine : null;
         },
+        // Remove the applied filter and reset it in the state
         removedAppliedFilter: (state, action) => {
             const filter = action.payload;
 
@@ -38,6 +47,7 @@ export const filtersSlice = createSlice({
                 state.cuisine = "Any";
             }
         },
+        // Reset all filters to their initial states
         resetFilters: state => {
             state.sortBy = null;
             state.appliedSortByFilter = null;
@@ -47,6 +57,7 @@ export const filtersSlice = createSlice({
     }
 });
 
+// Export actions from the filters slice
 export const {
     updateSortFilter,
     resetSortFilter,
@@ -57,8 +68,11 @@ export const {
     resetFilters
 } = filtersSlice.actions;
 
+// Export selectors for the filters slice
 export const selectSortFilter = state => state.filters.sortBy;
 export const selectCuisineFilter = state => state.filters.cuisine;
 export const selectAppliedSortFilter = state => state.filters.appliedSortByFilter;
 export const selectAppliedCuisineFilter = state => state.filters.appliedCuisineFilter;
+
+// Export the filters slice reducer
 export default filtersSlice.reducer;

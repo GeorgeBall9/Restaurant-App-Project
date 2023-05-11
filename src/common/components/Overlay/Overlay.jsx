620499653bf6450f96f35ab6271d3a6089eeb087
ryhhill1998 <ryhhill1998@outlook.com>
2023-05-11 16:51:55 +0100

    commented overlay

--- src/common/components/Overlay/Overlay.jsx
+++ src/common/components/Overlay/Overlay.jsx
@@ -1,3 +1,10 @@
+/*
+Description: Overlay component for use when any popup is displayed to make content below unclickable
+Author: Ryan Henzell-Hill
+Contact: ryan.henzell-hill@outlook.com
+*/
+
+// stylesheet
 import "./Overlay.css";
 
 const Overlay = ({handleClick}) => {

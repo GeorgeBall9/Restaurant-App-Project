657f40445d6aacb11d3666d6b9a0228616db541b
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-05-11 11:49:47 +0100

    c

--- src/routes/DetailsPage/InteractionsView/InteractionsView.jsx
+++ src/routes/DetailsPage/InteractionsView/InteractionsView.jsx
@@ -1,4 +1,13 @@
+/*
+ Description: This file contains the InteractionsView component, which is a sub-component of the DetailsPage component.
+ It renders the interactions section of the DetailsPage component.
+ Author: Ryan Henzell-Hill
+ Contact: ryan.henzell-hill@outlook.com
+ */
+
+// stylesheet 
 import "./InteractionsView.css";
+// Imports
 import {faBookmark, faCheckCircle, faHeart} from "@fortawesome/free-regular-svg-icons";
 import Interaction from "./Interaction/Interaction";
 
@@ -6,7 +15,7 @@ const InteractionsView = ({recommendations, bookmarks, checkIns}) => {
     return (
         <div className="interactions-view">
             <h2>Interactions</h2>
-
+{/*Render multiple Interaction components for each interaction section*/}
             <div>
                 <Interaction count={recommendations} icon={faHeart}/>
 

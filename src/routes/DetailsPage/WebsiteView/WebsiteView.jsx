9dab90e103107c8936e536eaf173e900cb250bac
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-05-11 11:57:22 +0100

    c

--- src/routes/DetailsPage/WebsiteView/WebsiteView.jsx
+++ src/routes/DetailsPage/WebsiteView/WebsiteView.jsx
@@ -1,4 +1,12 @@
+/*
+ Description: This file contains the WebsiteView component, which is a sub-component of the DetailsPage component.
+    It renders the website section of the DetailsPage component.
+ Author: Ryan Henzell-Hill
+ Contact: ryan.henzell-hill@outlook.com
+ */
+// stylesheet
 import "./WebsiteView.css";
+// Imports
 import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowUpRightFromSquare} from "@fortawesome/free-solid-svg-icons";
@@ -18,7 +26,7 @@ const WebsiteView = ({url}) => {
     return (
         <div className="website-view">
             <h2>Website</h2>
-
+{/*Render a link to the website, with the domain name as the text and an icon to the right*/}
             <Link to={url}>
                 {getDomainName(url)}
                 <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon"/>

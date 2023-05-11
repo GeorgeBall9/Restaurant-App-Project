e8f4935826853974f0b2c497a31ad29df2ae1033
GeorgeBall9 <b9029377@newcastle.ac.uk>
2023-05-11 11:42:13 +0100

    com

--- src/routes/DetailsPage/BannerView/ShareButton/ShareButton.jsx
+++ src/routes/DetailsPage/BannerView/ShareButton/ShareButton.jsx
@@ -1,4 +1,11 @@
+/*
+ Description: ShareButton component for the DetailsPage component. Handles a user sharing a restaurant.
+ Author: Ryan Henzell-Hill
+ Contact: ryan.henzell-hill@outlook.com
+ */
+//stylesheet
 import "./ShareButton.css";
+// Imports
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faShareFromSquare} from "@fortawesome/free-regular-svg-icons";
 import {faCheck, faCircleCheck, faLink, faXmark} from "@fortawesome/free-solid-svg-icons";
@@ -10,7 +17,7 @@ const ShareButton = ({id, style}) => {
 
     const [popupIsVisible, setPopupIsVisible] = useState(false);
     const [linkCopied, setLinkCopied] = useState(false);
-
+// OnClick handle functions
     const handleClick = () => {
         console.log("sharing to social media");
         setPopupIsVisible(true);
@@ -30,7 +37,7 @@ const ShareButton = ({id, style}) => {
             <button className="share-button" onClick={handleClick}>
                 <FontAwesomeIcon icon={faShareFromSquare} className="icon" style={style}/>
             </button>
-
+            {/* Render share popup */}
             {popupIsVisible && (
                 <div className="share-popup">
                     <h3>
@@ -44,7 +51,7 @@ const ShareButton = ({id, style}) => {
                             <FontAwesomeIcon icon={faXmark} className="icon"/>
                         </button>
                     </h3>
-
+                {/* Render share buttons and navigate user to respective destination*/}
                     <div className="buttons-container">
                         <div className="button-container">
                             <Link
